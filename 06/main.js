754800e00f940323a1ef206b10ce0ca0f0e6e71c
ILIA <okylevich@mail.ru>
2018-10-30 15:02:24 +0300

    add 08 and modify 06

--- 06/main.js
+++ 06/main.js
@@ -2,6 +2,11 @@ var array1 = [123, "string", true, {value: 321}, null],
     array2 = ["first", "second", "last"],
     result = [];
 
-result = [...array1, ...array2];
+    for (var i = 0; i < array1.length; i++) {
+        result [i] = array1[i];
+    }
+    for (var i = 0; i < array2.length; i++) {
+        result [i + array1.length] = array2[i];
+    }
 
 console.log("result: ", result);
